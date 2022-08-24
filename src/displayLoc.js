9a16553784cc4622a75642485711fb964a616dc6
Somtochukwu Francis <somtofrancis5@gmail.com>
2022-08-24 15:26:30 -0800

    Refactor to take argument

--- src/displayLoc.js
+++ src/displayLoc.js
@@ -1,11 +1,11 @@
 let locationContainer = document.getElementById('Location');
 
-function getLocation(){
+function getLocation(city){
     let form = document.createElement('form');
     let inputLocation = document.createElement('input');
     inputLocation.setAttribute('id', 'inputLocation');
     inputLocation.setAttribute('required', '');
-    inputLocation.setAttribute('placeholder', 'London');
+    inputLocation.setAttribute('placeholder', city);
     let submitButton = document.createElement('button');
     submitButton.setAttribute('type', 'submit');
     form.appendChild(inputLocation);
