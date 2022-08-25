629570c36d4996b524ba6e149b61dbb3c4d4e40e
Somtochukwu Francis <somtofrancis5@gmail.com>
2022-08-24 16:52:51 -0800

    Write function to fetch weather data

--- src/displayLoc.js
+++ src/displayLoc.js
@@ -1,16 +1,34 @@
 let locationContainer = document.getElementById('Location');
+import {attributeSetter} from './DOMmethods';
 
 function displayLoc(city){
     let form = document.createElement('form');
     let inputLocation = document.createElement('input');
-    inputLocation.setAttribute('id', 'inputLocation');
-    inputLocation.setAttribute('required', '');
-    inputLocation.setAttribute('placeholder', city);
+    inputLocation = attributeSetter(inputLocation, ['id', 'inputLocation'], ['required', ''], ['placeholder', city]);
     let submitButton = document.createElement('button');
     submitButton.setAttribute('type', 'submit');
     form.appendChild(inputLocation);
     form.appendChild(submitButton);
     locationContainer.appendChild(form);
-}
+    form.addEventListener('submit', (e)=>
+    {
+        getData(inputLocation.value);
+        e.preventDefault();
+    }
+    );
+};
 
+
+async function getData(location){
+    let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=7a05c54f9c2f27e1237267d2d7d1c58f`, 
+    {
+        mode: 'cors'
+    });
+    response = await response.json();
+    console.log(response);
+    return response;
+}
+getData().catch((response)=>{
+    alert('Error');
+})
 export default displayLoc;
