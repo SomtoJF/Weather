da99cdc1a1057d8514870ea24ada0ce2dd1589ba
Somtochukwu Francis <somtofrancis5@gmail.com>
2022-08-27 02:35:56 -0800

    Manipulate DOM to display weather icons

--- src/displayTemp.js
+++ src/displayTemp.js
@@ -1,17 +1,29 @@
 let weatherContainer = document.getElementById('Weather');
+import {changeIcon} from './background';
 import './displayTemp.css';
 
-function displayTemp (temperature, Weather){
+function displayTemp (main, tempAndWeather){
     weatherContainer.innerHTML = '';
-    for(let i = 0; i < arguments.length; i++){
-        if(arguments[i] !== ''){
-            let parameter = arguments[i];
+    for(let i = 0; i < tempAndWeather.length; i++){
+        if(tempAndWeather[i] !== ''){
             let div = document.createElement('div');
             if(i == 0){
-                parameter = (((parameter * 100) - 27315)/100).toFixed(2);
-                parameter += '°C';
+                tempAndWeather[i] = (((tempAndWeather[i] * 100) - 27315)/100).toFixed(2);
+                tempAndWeather[i] += '°C';
             };
-            div.textContent = parameter;
+            if(i == 1){
+                let icondiv = document.createElement('div');
+                changeIcon(icondiv, main);
+                icondiv.setAttribute('id', 'icon');
+                div.appendChild(icondiv);
+
+                let weatherdiv = document.createElement('div');
+                weatherdiv.textContent = tempAndWeather[i];
+                div.appendChild(weatherdiv);
+                weatherContainer.appendChild(div);
+                continue;
+            }
+            div.textContent = tempAndWeather[i];
             weatherContainer.appendChild(div);
         }
     };
