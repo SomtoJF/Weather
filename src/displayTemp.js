c3c65ad3d5598c17d53af8d0ff7d4665c1539fb3
Somtochukwu Francis <somtofrancis5@gmail.com>
2022-08-27 01:24:54 -0800

    convert temperature from kelvin to celcius

--- src/displayTemp.js
+++ src/displayTemp.js
@@ -2,14 +2,18 @@ let weatherContainer = document.getElementById('Weather');
 import './displayTemp.css';
 
 function displayTemp (temperature, Weather){
+    weatherContainer.innerHTML = '';
     for(let i = 0; i < arguments.length; i++){
-        let div = document.createElement('div');
-        div.textContent = arguments[i];
-        weatherContainer.appendChild(div);
-
-        if(i == 0){
-            div.textContent += '°C'
-        };
+        if(arguments[i] !== ''){
+            let parameter = arguments[i];
+            let div = document.createElement('div');
+            if(i == 0){
+                parameter = (((parameter * 100) - 27315)/100).toFixed(2);
+                parameter += '°C';
+            };
+            div.textContent = parameter;
+            weatherContainer.appendChild(div);
+        }
     };
 };
 export default displayTemp;
