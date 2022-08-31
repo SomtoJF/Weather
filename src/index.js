0f77f9fffe19b73f3e655d96508bd4fb80573eae
Somtochukwu Francis <somtofrancis5@gmail.com>
2022-08-31 01:19:55 +0100

    Display Weather of last location searched

--- src/index.js
+++ src/index.js
@@ -3,5 +3,11 @@ import {default as displayLoc} from './displayLoc';
 import {default as displayTemp} from "./displayTemp";
 import {default as displayCondition} from './displayCondition';
 import {getData} from './displayLoc';
+import {default as storageAvailable} from './storage';
 
-getData('Lagos');
+if(storageAvailable('localStorage') && localStorage.getItem('lastSearch')){
+    getData(localStorage.getItem('lastSearch'));
+}
+else{
+    getData('Lagos');
+};
